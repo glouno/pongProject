1b215438c6c92b6db7f1da93e8d6e6d9160ff4d4
glouno <p.beglin18@ejm.org>
2022-11-28 16:55:48 +0100

    Controls & Game seperate files init

---
+++ controls.py
@@ -0,0 +1,1 @@
+# Here we'll use the opencv module to send the control inputs to the game
---
+++ game.py
@@ -0,0 +1,3 @@
+# Here we'll make the normal game using UP and DOWN arrows 
+# to later be replaced by the opencv controls
+
