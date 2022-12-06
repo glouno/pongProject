3cf8265059f56175c0adbb45dd37650af263fac0
Raphaela237 <raphaellaessombonno@gmail.com>
2022-12-06 20:04:16 +0100

    configuration menu

--- menu.py
+++ menu.py
@@ -292,17 +292,17 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, (182, 143, 64))
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         Bouton_play = Button(image=pygame.image.load("image jeu/Play Rect.png"), pos=(640, 250), 
-                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="PLAY", font=get_font(75), base_color=(215,252,212), hovering_color="White")
         Bouton_help = Button(image=pygame.image.load("image jeu/aide Rect.png"), pos=(640, 400), 
-                            text_input="HELP", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="HELP", font=get_font(75), base_color=(215,252,212), hovering_color="White")
         Bouton_score = Button(image=pygame.image.load("image jeu/aide Rect.png"), pos=(640, 550), 
-                            text_input="SCORE", font=get_font(75), base_color="#d7fcd4", hovering_color="White")                   
+                            text_input="SCORE", font=get_font(75), base_color=(215,252,212), hovering_color="White")                   
         Bouton_quitter = Button(image=pygame.image.load("image jeu/Quit Rect.png"), pos=(640, 700), 
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="QUIT", font=get_font(75), base_color=(215,252,212), hovering_color="White")
 
         fenetre.blit(MENU_TEXT, MENU_RECT)
 
