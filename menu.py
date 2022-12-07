080c5a815dc17504ab84a185fef3c2a395ca292c
Raphaela237 <raphaellaessombonno@gmail.com>
2022-12-07 16:25:59 +0100

    raphaela detruit tout le code

--- menu.py
+++ menu.py
@@ -13,81 +13,6 @@ icone = pygame.image.load('image jeu/ICONE.png')
 pygame.display.set_icon(icone)
 
 background = pygame.image.load('image jeu/BACK2.jpg')
-'''
-La classe Sprite est la classe de base  de tous les elements graphiques visibles de la fenetre
-De ce fait, notre classe Player va heriter de tous les attributs et methode de la classe 
-Sprite notamment le methode super
-def _init_ quant à lui est un un constructeur qui est initialisé lors de la création d'une classe
-Et ce constructeur va se charrger de contenir tous les attributs de notte classe Player
-Enfin le mot clé self directement generé par la fonction est un mot clé qui va nous permettre de faire
-reference à notre objet tout au long de la fonction
-'''
-class Player (pygame.sprite.Sprite):
-    def __init__(self, color, width, height ) :
-        super().__init__() 
-        self.color = color
-        self.width = width
-        self.height = height
-        # Maintenant on va dessiner et configurer l'espace sur lequel on va créer nos différents joueurs
-        self.image = pygame.Surface([width, height ])
-        self.image.fill((255,255,255))
-        self.image.set_colorkey((255,255,255))
-
-        # On dessine ensuite le rectangle sur la surface generé plus haut
-        pygame.draw.rect(self.image, self.color, [0,0, self.width, self.height ])
-
-        # Enfin on récupere l'image conçu dans l'objet rect afin de le deplacer facilement sur le fenetre
-        self.rect = self.image.get_rect()
-
-
-
-    # fonction des déplacements des joueurs
-    def monter(self, pixels):
-        self.rect.y -= pixels
-        # on veut s'assurer que le joueur reste dans les liùmites de la fenetre
-        if (self.rect.y < 0):
-            self.rect.y = 0
-
-    def descendre(self, pixels):
-        self.rect.y += pixels
-        if self.rect.y > (HAUTEUR - self.height):
-            self.rect.y = (HAUTEUR - self.height)
-# Creation de la classe Balle
-
-class Balle (pygame.sprite.Sprite):
-    def __init__(self, color, width, height, rayon) :
-        super().__init__()
-        self.color = color
-        self.width = width
-        self.height = height
-        self.rayon = rayon
-        '''
-        Tout comme pour les joueurs on va dessiner la surface qui va contenir le cercle et ensuite
-        le recuperer dans l'objet rect pour pouvoir le déplacer sur la fenetre
-        '''
-        self.image = pygame.Surface([width, height])
-        self.image.fill((255,255,255))
-        self.image.set_colorkey((255,255,255))
-        #vitesse aleatoire de la balle
-        self.vitesse = [2 , 4]
-
-        # On dessine le cercle
-        pygame.draw.circle(self.image, self.color,(self.width//2, self.height//2), self.rayon)
-        self.rect = self.image.get_rect()
-    
-    # C'est cette methode qui va faire bouger la balle
-    def update (self):
-        self.rect.centerx += self.vitesse[0]
-        self.rect.centery += self.vitesse[1]
-    
-    '''
-    cette derniere methode va permettre de definir une methode qui inverse la vitesse de la balle lorsque celle-ci rencontre la 
-    raquette d'un joueur
-    '''
-    
-    def rebond(self):
-        self.vitesse[0] *= -1
-        self.vitesse[1] *= -1
 
 class Button():
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
@@ -120,109 +45,13 @@ class Button():
 			self.text = self.font.render(self.text_input, True, self.base_color)
 
 
-def get_font(size): # Returns Press-Start-2P in the desired size
+def get_font(size): 
     return pygame.font.Font('image jeu/font.ttf', size)
 
 def play():
     
     main.main()
 
-    '''
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
-        pygame.display.set_caption("jeu")
-
-        
-        fenetre.fill((255,255,255))
-        # dessine une ligne au milieu de la fenetre
-        pygame.draw.line(fenetre,(50,50,50), [LARGEUR//2, 0 ],[LARGEUR//2, HAUTEUR ], 5)
-
-        Player_1= Player((50,50,50), LARGEUR_JOUEUR, HAUTEUR_JOUEUR)
-        Player_1.rect.x = 0
-        Player_1.rect.y = (HAUTEUR - HAUTEUR_JOUEUR) // 2
-        Player_2 = Player((50,50,50), LARGEUR_JOUEUR, HAUTEUR_JOUEUR)
-        Player_2.rect.x = (LARGEUR - LARGEUR_JOUEUR)
-        Player_2.rect.y = (HAUTEUR - HAUTEUR_JOUEUR) //2
-
-        balle = Balle((50,50,50), 40, 40, 20)
-        balle.rect.centerx = LARGEUR //2
-        balle.rect.centery = HAUTEUR // 2  
-
-        score1 = 0
-        score2 = 0
-        font = pygame.font.Font('image jeu/AtariClassicExtrasmooth-LxZy.ttf',74)
-        message1 = font.render ( str(score1), 1,(50,50,50))
-        message2 = font.render ( str(score2), 1,(50,50,50)) 
-        
-        #Étant donné que les différentes instances de la classe Player hérite aussi de la classe Sprite
-        #il faut créér une liste qui va regrouper tous les elements de cette et les ajouter à chaque fois sur notre ecran 
-        
-        # ajouter des elements graphiques à la fenetre 
-        all_sprites_list = pygame.sprite.Group()
-        all_sprites_list.add(Player_1)
-        all_sprites_list.add(Player_2)
-        all_sprites_list.add(balle) 
-
-        
-
-
-        # dessiner les différents objets à l'ecran
-        all_sprites_list.draw(fenetre)
-
-        # dessiner le score
-        fenetre.blit ( message1 , [LARGEUR//4, 10 ] )
-        fenetre.blit ( message2, [3*LARGEUR//4, 10 ] )
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        # on va recuperer les touches saisies par l'utilisateur dans une variable
-        Keys = pygame.key.get_pressed()
-        if Keys[pygame.K_UP]:
-            Player_2.monter(10)
-        if Keys[pygame.K_DOWN]:
-            Player_2.descendre(10)
-        if Keys[pygame.K_w]:
-            Player_1.monter(10)
-        if Keys[pygame.K_s]:
-            Player_1.descendre(10)
-    
-        balle.update()
-        # gestion des collisions et incrementation du score
-        if balle.rect.centerx > (LARGEUR - LARGEUR_JOUEUR):
-            score1 += 1
-            (balle.rect.centerx, balle.rect.centery) = (LARGEUR //2, HAUTEUR //2)
-            balle.vitesse[0] *= -1
-        if balle.rect.centerx < LARGEUR_JOUEUR:
-            score2 += 1
-            (balle.rect.centerx, balle.rect.centery) = (LARGEUR //2,HAUTEUR //2)
-            balle.vitesse[0] *= -1
-        if balle.rect.centery > (HAUTEUR - LARGEUR_JOUEUR):
-            balle.vitesse[1] *= -1
-        if balle.rect.centery < LARGEUR_JOUEUR:
-            balle.vitesse[1] *= -1   
-    
-        
-        #Sprite possede une methode collide.mask qui permet de dire si deux objets sont entrés en collisions
-        #Nous allons l'utiliser ici pour determiner lorsqu'une raquette frappe la balle et ainsi inverser
-        #la vitesse de celle-ci grave à la fonction rebond 
-        
-        if pygame.sprite.collide_mask(balle, Player_1) or pygame.sprite.collide_mask(balle, Player_2):
-            balle.rebond()
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            
-            #if event.type == pygame.MOUSEBUTTONDOWN:
-                #if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    #main_menu()
-            
-        pygame.display.update()
-
-        '''
 
 def aide():
     while True:
